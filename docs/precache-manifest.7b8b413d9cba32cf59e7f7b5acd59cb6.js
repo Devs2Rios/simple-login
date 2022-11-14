14c076c2992369fd2add6c6cf516d14b0a4afebf
eduairet <hola@eduairet.com>
2022-11-14 14:18:20 -0600

    first commit

---
+++ docs/precache-manifest.7b8b413d9cba32cf59e7f7b5acd59cb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abc7fce7815e49051c90",
+    "url": "/static/css/main.230887aa.chunk.css"
+  },
+  {
+    "revision": "abc7fce7815e49051c90",
+    "url": "/static/js/main.abc7fce7.chunk.js"
+  },
+  {
+    "revision": "84b1a5ba8a3d2689c8bd",
+    "url": "/static/js/1.84b1a5ba.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af7c8fbe112853bf15e11f8644e64f29",
+    "url": "/index.html"
+  }
+];
